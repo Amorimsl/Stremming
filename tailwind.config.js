4ca79362eb9d8ebe14bcd572b0fcded29a0f53e1
lucas amorim <lucasamorim_10_@hotmail.com>
2024-06-25 13:37:15 -0300

    Ajuste no header-footer

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Work Sans', 'sans-serif'],
+        lato: ['Lato'],
+      },
+      colors: {
+        'custom-gray-900': 'rgba(14, 16, 18, 1)',
+        'custom-gray-800': 'rgba(14, 16, 18, 0)',
+        'custom-neutral': 'rgba(16, 17, 22, 1)',
+        'custom-border': 'rgba(255, 255, 255, 0.12)',
+      },
+    },
+  },
+  plugins: [],
+};
